13bc575cebf1d59ec24f0c88b61f7542ba5a6933
Pablo Manuel Fernández Velázquez <163730673+ProfitCoder@users.noreply.github.com>
2026-04-29 21:59:57 +0200

    Update Base de datos Proyecto.sql

--- DataBase/Base de datos Proyecto.sql
+++ DataBase/Base de datos Proyecto.sql
@@ -1,7 +1,3 @@
-CREATE DATABASE Gestion_Proyectos_infor;
-
-USE DATABASE Gestion_Proyectos_infor;
-
 ---------------------
 -- Creación de Tablas
 ---------------------
@@ -38,18 +34,70 @@ CREATE TABLE Informaticos(
 	Tipos_informaticos VARCHAR2(30),
 	Horas_proyecto NUMBER,
 	Coste_dedicacion VARCHAR2(30),
-	Lenguaje_Programación VARCHAR2(30),
     FOREIGN KEY (Codigo_Empleado) REFERENCES Empleado(Codigo_Empleado)
 );
 
+CREATE TABLE Programadores(
+    Codigo_Empleado NUMBER PRIMARY KEY,
+    Lenguaje_Programación VARCHAR2(30),
+    FOREIGN KEY (Codigo_Empleado) REFERENCES Empleado(Codigo_Empleado)
+);
 
+CREATE TABLE Analistas(
+    Codigo_Empleado NUMBER PRIMARY KEY,
+    FOREIGN KEY (Codigo_Empleado) REFERENCES Empleado(Codigo_Empleado)
+);
 
 CREATE TABLE Productos(
-	
+    Codigo_Producto NUMBER PRIMARY KEY,
+    Nombre VARCHAR2(30),
+    Descripcion VARCHAR2(50),
+    Estado VARCHAR2(30),
+    Analista_Responsable NUMBER PRIMARY KEY,
+    FOREIGN KEY (Analista_Responsable) REFERENCES Analistas(Codigo_Empleado)
+);	
 
 CREATE TABLE Fases_de_Proyecto(
-	Codigo_Proyecto NUMBER Primary Key,
-	Codigo_Prod NUMBER Primary key),
+	Codigo_Proyecto NUMBER PRIMARY KEY,
+	Codigo_Producto NUMBER PRIMARY KEY,
+    Codigo_Recurso NUMBER PRIMARY KEY,
 	FOREIGN KEY (Codigo_Proyecto) REFERENCES Proyecto (Codigo_Proyecto),
-	FOREIGN KEY (Codigo_Productos) REFERENCES Productos (Codigo_Productos)
+	FOREIGN KEY (Codigo_Producto) REFERENCES Productos (Codigo_Producto),
+    FOREIGN KEY (Codigo_Recurso) REFERENCES Recursos (Codigo_Recurso)
 );
+
+CREATE TABLE Software(
+    Codigo_Producto NUMBER PRIMARY KEY,
+    Tipo VARCHAR2(30),
+    FOREIGN KEY (Codigo_Producto) REFERENCES Productos (Codigo_Producto)
+);
+
+CREATE TABLE Prototipo(
+    Codigo_Producto NUMBER PRIMARY KEY,
+    Version VARCHAR2(30),
+    Ubicacion VARCHAR2(50),
+    FOREIGN KEY (Codigo_Producto) REFERENCES Productos (Codigo_Producto)
+);
+
+CREATE TABLE Gastos(
+    Codigo_Gastos NUMBER PRIMARY KEY,
+    Descipcion VARCHAR2(50),
+    Fecha DATE,
+    Importe NUMBER,
+    Tipo_Gasto VARCHAR2(30),
+    Codigo_Proyecto NUMBER PRIMARY KEY,
+	Codigo_Producto NUMBER PRIMARY KEY,
+	FOREIGN KEY (Codigo_Proyecto) REFERENCES Proyecto (Codigo_Proyecto),
+	FOREIGN KEY (Codigo_Producto) REFERENCES Productos (Codigo_Producto)
+);
+
+CREATE TABLE Recursos(
+    Codigo_Recurso NUMBER PRIMARY KEY,
+    Nombre VARCHAR2(50),
+    Descripcion VARCHAR2(50),
+    Tipo VARCHAR2(30)
+);
+    
+--------------------------
+--INSERT EN TABLAS
+--------------------------
